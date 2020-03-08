64c7d76e0fda971ca1774a70f0794007a6d8bd88
Kean <chankeanyeow@gmail.com>
2020-03-08 15:29:20 +0800

    first commit
    Signed-off-by: Kean <chankeanyeow@gmail.com>

---
+++ src/app/products.ts
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    size: '4.4 inch',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    size: '4.8 inch',
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 400,
+    description: 'phone standard description'
+  },
+    {
+    name: 'Phone 4',
+    price: 701,
+    size: '4.5 inch'
+  }
+
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
